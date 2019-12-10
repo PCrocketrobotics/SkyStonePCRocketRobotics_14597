1d453eb01e4f65972a4627d6b4dc988fa4002ed7
PC Rocket Robotics <admin@pcrocketrobotics.com>
2019-12-09 23:15:49 -0500

    Init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_2.java
@@ -0,0 +1,198 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous", group="Pushbot")
+public class Autonomous_2 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    RR_Hardware         robot   = new RR_Hardware();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final int     COUNTS_PER_MOTOR_REV    = 28 ;    // Motor with 1:1 gear ratio
+    static final int     DRIVE_GEAR_REDUCTION    = 12 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH_DOUBLE         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
+
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                robot.left_rear.getCurrentPosition(),
+                robot.right_rear.getCurrentPosition(),
+                robot.left_front.getCurrentPosition(),
+                robot.right_front.getCurrentPosition());
+
+        telemetry.update();
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        if (opModeIsActive()) {
+            robot.left_front.setDirection(DcMotor.Direction.REVERSE);
+
+            robot.left_rear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.left_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.right_rear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.right_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.left_rear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.right_rear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                                                                                    //The number multiplied by "COUNTS_PER_INCH_DOUBLE" is the inches the robot will travel.
+            robot.left_front.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * 39)); //39 = the number of inches we want to travel.
+            robot.right_front.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * 39));
+            robot.right_rear.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * 39));
+            robot.left_rear.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * 39));
+
+            robot.left_front.setPower(0.5);
+            robot.left_rear.setPower(0.5);
+            robot.right_front.setPower(0.5);
+            robot.right_rear.setPower(0.5);
+
+            while (robot.left_front.isBusy() || robot.left_rear.isBusy() || robot.right_rear.isBusy() || robot.right_front.isBusy()) {
+
+            }
+
+            robot.servo_right.setPosition(0.3214);
+            robot.servo_left.setPosition(0.3214);
+
+            robot.left_front.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * -39));
+            robot.right_front.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * -39));
+            robot.right_rear.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * -39));
+            robot.left_rear.setTargetPosition((int)(COUNTS_PER_INCH_DOUBLE * -39));
+
+            robot.left_front.setPower(0.5);
+            robot.left_rear.setPower(0.5);
+            robot.right_front.setPower(0.5);
+            robot.right_rear.setPower(0.5);
+
+            while (robot.left_front.isBusy() || robot.left_rear.isBusy() || robot.right_rear.isBusy() || robot.right_front.isBusy()) {
+
+            }
+
+            robot.servo_right.setPosition(-0.3214);
+            robot.servo_left.setPosition(-0.3214);
+
+            int blueValue = robot.colorSensor.blue();
+
+            while   (blueValue < 90)   {
+
+
+                robot.left_front.setPower(0.2);
+                robot.right_front.setPower(-0.2);
+                robot.right_rear.setPower(0.2);
+                robot.left_rear.setPower(-0.2);
+                blueValue = robot.colorSensor.blue();
+            }
+
+
+        }
+        robot.left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.right_rear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.left_rear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+}
+
+
+}
+
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+
+
+
+
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_Hardware.java
@@ -0,0 +1,132 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+public class RR_Hardware
+{
+    /* Public OpMode members. */
+    public DcMotor  left_rear   = null;
+    public DcMotor  right_rear  = null;
+    public DcMotor  left_front   = null;
+    public DcMotor  right_front    = null;
+    public Orientation lastAngles = new Orientation();
+    public DistanceSensor distanceSensor;
+    public ColorSensor colorSensor;
+    public BNO055IMU imu;
+    public Servo servo_left = null;
+    public Servo servo_right = null;
+    public static final double MID_SERVO = 0;
+    public static final double max_pos = 0.25;
+    public static final double start_pos = 0;
+   // public DcMotor  leftArm     = null;
+    //public Servo    leftClaw    = null;
+    //public Servo    rightClaw   = null;
+
+    //public static final double ARM_UP_POWER    =  0.45 ;
+    //public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public RR_Hardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
+        // Define and Initialize Motors
+        left_front        = hwMap.get(DcMotor.class, "leftFront");
+        left_rear         = hwMap.get(DcMotor.class, "leftRear");
+        right_front       = hwMap.get(DcMotor.class, "rightFront");
+        right_rear        = hwMap.get(DcMotor.class, "rightRear");
+        servo_left        = hwMap.get(Servo.class, "servoLeft");
+        servo_right       = hwMap.get(Servo.class,"servoRight");
+
+        colorSensor       = hwMap.get(ColorSensor.class, "sensor_color_distance");
+
+        left_rear.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        left_front.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        right_rear.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        right_front.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        left_front.setPower(0);
+        right_front.setPower(0);
+        left_rear.setPower(0);
+        right_rear.setPower(0);
+        //leftArm.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_rear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_rear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        servo_right.scaleRange(0, 1);
+        servo_left.scaleRange(0, 1);
+
+
+
+        //TO DO: SET MOTORS TO COAST AND BREAK
+    }
+ }
+
